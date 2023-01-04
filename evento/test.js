eddc743dd8411094c85130f9c3c31509fa53ad9b
Ivan Altamirano <pedrotrill@gmail.com>
2023-01-03 22:05:15 -0300

    arreglando gitignore

--- evento/test.js
+++ evento/test.js
@@ -1,4 +1,4 @@
-import { evento } from './index.js';//const { evento } = require("./index");
+const { evento } = require("./index");//import { evento } from './index.js';
 
 evento.emit('prueba2', {name: 'Pepito'});
 
