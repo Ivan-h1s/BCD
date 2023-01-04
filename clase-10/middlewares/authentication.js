5586a68dab6e96e156ef8878733d46e52770bbde
Ivan Altamirano <pedrotrill@gmail.com>
2023-01-04 03:27:28 -0300

    clase10

---
+++ clase-10/middlewares/authentication.js
@@ -0,0 +1,9 @@
+function authentication(req, res, next) {
+    //if(true) {
+        next();
+    //} else {
+    //    res.end('Usuario no autorizado');
+   // }
+}
+
+module.exports = authentication;
---
+++ clase-10/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const userRoute = require('./routes/user');
+const log = require('./middlewares/log');
+const authentication = require('./middlewares/authentication');
+
+app.use(log);
+app.use(express.json());//app.use(bodyParser.json());
+//ruta agregada
+app.get('/', (req, res) => {
+    res.send('Hola mundo desde Express!')
+});
+
+app.use('/user', authentication, userRoute);
+
+mongoose.connect('mongodb://127.0.0.1:27017/clase-10', (error) => {
+    if(error) {
+        console.log('Error en la conexion a MongoDB', error);
+    } else {
+        console.log('Conexion exitosa con MongoDB');
+    }
+})
+
+app.listen(3000);
